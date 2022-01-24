89f5d6462f3936dc5387747f08df04282bbfa82c
Raju <kalyanraju541@gmail.com>
2022-01-24 09:09:13 +0000

    upto-fetch-videos

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,14 @@
 import {Component} from 'react'
 import Cookies from 'js-cookie'
+import {BiSearch} from 'react-icons/bi'
 import ThemeContext from '../../context/ThemeContext'
 import Header from '../Header'
 import SideNavbar from '../SideNavBar'
 import './index.css'
 
 class Home extends Component {
+  state = {videosList: [], searchInput: ''}
+
   componentDidMount() {
     this.getHomeVideos()
   }
@@ -20,8 +23,19 @@ class Home extends Component {
       },
     }
     const response = await fetch(homeVideosApiUrl, options)
-    const data = await response.json()
-    console.log(data)
+    if (response.ok === true) {
+      const data = await response.json()
+      const {videos} = data
+      const updatedData = videos.map(eachItem => ({
+        id: eachItem.id,
+        channel: eachItem.channel,
+        publishedAt: eachItem.published_at,
+        thumbnailUrl: eachItem.thumbnail_url,
+        title: eachItem.title,
+        viewCount: eachItem.view_count,
+      }))
+      console.log(updatedData)
+    }
   }
 
   render() {
@@ -34,6 +48,27 @@ class Home extends Component {
               <Header />
               <div className="home-bg-content-container">
                 <SideNavbar />
+                <div className="home-bg-videos-content-container">
+                  <div className="banner-section">
+                    <img
+                      src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png"
+                      className="banner-logo"
+                      alt="website logo"
+                    />
+                    <p>Buy Nxt Watch Premium prepaid plans with UPI</p>
+                    <button className="banner-btn" type="button">
+                      GET IT NOW
+                    </button>
+                  </div>
+                  <div className="home-videos-container">
+                    <div className="input-search-container">
+                      <input className="input-search-field" type="search" />
+                      <button className="search-btn" type="button">
+                        <BiSearch />
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           )
--- src/components/SideNavBar/index.js
+++ src/components/SideNavBar/index.js
@@ -7,15 +7,40 @@ const SideNavBar = () => (
     {value => {
       const {sideBarOptionsList, activeTabId, isThemeLight} = value
       const renderLightThemeSideNavBar = () => (
-        <ul className="sidebar-container">
-          {sideBarOptionsList.map(eachItem => (
-            <SidebarOptionItem
-              key={eachItem.id}
-              details={eachItem}
-              isActive={activeTabId === eachItem.id}
-            />
-          ))}
-        </ul>
+        <div className="sidebar-container">
+          <ul className="sidebar-options-container">
+            {sideBarOptionsList.map(eachItem => (
+              <SidebarOptionItem
+                key={eachItem.id}
+                details={eachItem}
+                isActive={activeTabId === eachItem.id}
+              />
+            ))}
+          </ul>
+          <div>
+            <p>CONTACT US</p>
+            <div className="contact-us-logos-container">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/nxt-watch-facebook-logo-img.png"
+                alt="facebook logo"
+                className="contact-us-logo"
+              />
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/nxt-watch-twitter-logo-img.png"
+                alt="twitter logo"
+                className="contact-us-logo"
+              />
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/nxt-watch-linked-in-logo-img.png"
+                alt="linked in logo"
+                className="contact-us-logo"
+              />
+            </div>
+            <p className="contact-us-description">
+              Enjoy! Now to see your channels and recommendations!
+            </p>
+          </div>
+        </div>
       )
       const renderDarkThemeSideNavbar = () => {}
 
